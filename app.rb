9cdfc25d9e5a5b4b4994dccbb98c041442e7d936
Drew <drew@finstrom.us>
2015-05-01 12:29:14 -0700

    routes and views for word check; form fields in words view; integration spec for adding words (passed)

--- app.rb
+++ app.rb
@@ -9,3 +9,37 @@ get('/test') do
   @test_var = "Sinatra OK"
   erb(:test)
 end
+
+#redirect root route to /words route
+get('/') do
+  redirect to('/words')
+end
+
+#list all words
+#form feild to add a new word
+get('/words') do
+  @words = Word.all()
+  erb(:words)
+end
+
+#post method for adding a word
+post('/words') do
+  @words = Word.all()
+  new_word = Word.new({ :word => params.fetch('word') })
+  new_word.save()
+  erb(:words)
+end
+
+#list all definitions for a word
+#form to add a definition
+get('/words/:word/definitions') do
+  word = params.fetch('word')
+  erb(:definitions)
+end
+
+#post method for adding a definition
+post('/words/:word/definitions') do
+  word = params.fetch('word')
+  definition = params.fetch('definition')
+  erb(:definitions)
+end
--- spec/words_and_defs_integration_spec.rb
+++ spec/words_and_defs_integration_spec.rb
@@ -4,10 +4,22 @@ Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
 
-describe('Wordcount Integration Testing', {:type => :feature}) do
+describe('Sinatra pre-flight check', {:type => :feature}) do
   #framework test
   it('verifies a test route is getting to a test layout page') do
     visit('/test')
     expect(page).to have_content('Sinatra OK')
   end
+end
+
+
+describe('Word integration', {:type => :feature}) do
+
+  it('verfies addition of new words via form post to displayed list') do
+    visit('/words')
+    fill_in('word', :with => 'insanity')
+    click_button('Add New Word')
+    expect(page).to have_content('INSANITY')
+  end
+
 end
